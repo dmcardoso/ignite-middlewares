afbfd9115a19da9697288ce21b0e7753bdbacb34
Daniel Moreira Cardoso <dmcardoso.99@hotmail.com>
2021-04-29 09:19:04 -0300

    feat: Resolve challenge

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const userExists = users.find(user => user.username === username)
+
+  if(!userExists) {
+    return response.status(404).json({error: 'User doesn\'t exists'})
+  }
+
+  request.user = userExists
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+
+  if(!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({error: 'User not pro'})
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {id} = request.params
+
+  if(!validate(id)) {
+    return response.status(400).json({error: 'Id must be an uuid'})
+  }
+
+  const {username} = request.headers
+
+  const userExists = users.find(user => user.username === username)
+
+  if(!userExists) {
+    return response.status(404).json({error: 'User doesn\'t exists'})
+  }
+
+  const todo = userExists.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(404).json({error: 'Todo doesn\'t exists'})
+  }
+
+  request.todo = todo
+  request.user = userExists
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params
+
+  if(!validate(id)) {
+    return response.status(400).json({error: 'Id must be an uuid'})
+  }
+
+  const userExists = users.find(user => user.id === id)
+
+  if(!userExists) {
+    return response.status(404).json({error: 'User doesn\'t exists'})
+  }
+
+  request.user = userExists
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
